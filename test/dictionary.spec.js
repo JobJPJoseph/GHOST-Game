c6a1d019a0b75825683a05639a02e65ebbcbe792
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-04-19 23:23:44 -0400

    We added dictionary file

--- test/dictionary.spec.js
+++ test/dictionary.spec.js
@@ -6,3 +6,14 @@ chai.use(spies);
 const expect = chai.expect;
 
 const { dictionary } = require('../class/dictionary');
+
+describe('Dictionary Class', function () {
+
+    it('should initialize the dictionary class', function () {
+        expect(dictionary).to.exist;
+    });
+
+
+    // We need to test for a dictionary property exist
+    // Then check the keys length to make sure its from A to Z.
+});
